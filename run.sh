9202cf360074719e8f0be5e9b82a86338eff17a5
Sameer Narendran <sameern3@illinois.edu>
2026-05-04 01:01:50 +0000

    a

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -a
+source .venv/bin/activate
+source .env
+set +a
+
+
+LOG_FILE="model/train/train.log"
+
+echo "Starting training..."
+nohup python model/train.py > "$LOG_FILE" 2>&1 &
+
+PID=$!
+echo "Training started in background with PID $PID"
+echo "Logs: $LOG_FILE"
